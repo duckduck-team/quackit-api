6d2dd8436b80c382b8293731c4d6f0f3c28d4113
Ivan Sannikov <87535782+Petrel321@users.noreply.github.com>
2024-07-11 23:46:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -37,4 +37,4 @@ app.include_router(comment_router)
 app.include_router(unauthorized_router)
 
 if __name__ == "__main__":
-    uvicorn.run(app=app, host="127.0.0.1", port=8000)
+    uvicorn.run(app=app, host="0.0.0.0", port=8000)
